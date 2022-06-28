d52245a1717cfdca674a1c26ba704f4d517ed0e2
Michal Kubín <kubin.michal@gmail.com>
2022-06-28 14:41:16 +0200

    Založení projektu. Načtení zadání z externího souboru.

---
+++ nakupni_kosik.py
@@ -0,0 +1,26 @@
+"""
+nakupni_kosik.py: Úkol z Engeto akademie
+author: Michal Kubín
+email: kubin.michal@gmail.com
+discord: Michal Kubín #0577
+"""
+
+import zadani
+
+# Úvodní sekce s uvítáním a výpisem zboží
+
+print("Vitejte našem nákupním košíku!")
+print(zadani.oddelovac)
+print(zadani.nabidka)
+
+# Cyklus, který přidává zboží do košíku tak dlouho, jak uživatel zamýšlí
+
+    # .. pokud uživatel nezadá zboží z nabídky
+
+    # .. pokud zboží ještě v košíku není
+
+    # .. pokud se již vybrané zboží nachází v košíku
+
+    # .. pokud hodnota na indexu 1 odpovídá 0 (není skladem)
+
+# Finální výpis košíku po nákupu
---
+++ zadani.py
@@ -0,0 +1,25 @@
+# Zadané hodnoty
+kosik = dict()
+oddelovac = "=" * 40
+potraviny = {
+    "mleko": [30, 5],  # index '0'-> cena, index '1' -> pocet
+    "maso": [100, 1],
+    "banan": [30, 10],
+    "jogurt": [10, 5],
+    "chleb": [20, 5],
+    "jablko": [10, 10],
+    "pomeranc": [15, 10]
+}
+nabidka = """
++-----------+----------+
+| POTRAVINA |   CENA   |
++-----------+----------+
+| mleko     |    30,-  |
+| maso      |   100,-  |
+| banan     |    30,-  |
+| jogurt    |    10,-  |
+| chleb     |    20,-  |
+| jablko    |    10,-  |
+| pomenrac  |    15,-  |
++-----------+----------+
+"""
